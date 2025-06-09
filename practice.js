b12e7d4eb5ad8589ccc1c6b68d8c321a3218443e
sourav goyal <souravgoyal572@gmail.com>
2025-06-09 10:07:04 +0530

    Practice 090625

--- practice.js
+++ practice.js
@@ -2,10 +2,13 @@ console.log('welcome');
 
 // functions like setTimeout are example of callback function which allow asynchronous things in JS even though JS is synchronous.
 
-function attachEventListners(){
-    let count = 0;
-    document.getElementById("clickMe").addEventListener("click", function xyz(){
-    console.log("Button Clicked", ++count)
-})
-}
-attachEventListners();
+// function attachEventListners(){
+//     let count = 0;
+//     document.getElementById("clickMe").addEventListener("click", function xyz(){
+//     console.log("Button Clicked", ++count)
+// })
+// }
+// attachEventListners();
+
+//15
+
