6acf28d48a5a664430ea5d7a0ec79ae27f8b0548
sourav goyal <souravgoyal572@gmail.com>
2025-06-14 15:15:01 +0530

    140625

--- practice.js
+++ practice.js
@@ -4,7 +4,7 @@
 
 // function attachEventListners(){
 //     let count = 0;
-//     document.getElementById("clickMe").addEventListener("click", function xyz(){
+//    document.getElementById("clickMe").addEventListener("click", function xyz(){
 //     console.log("Button Clicked", ++count)
 // })
 // }
@@ -24,3 +24,38 @@
 
 //16
 //JS Engine
+//First JS Engine was SpirderMonkey
+//Code -> Parsing -> Compilation -> Execution
+
+//17
+// const radius = [3,1,2,4];
+
+// const area = function(radius) {     //Callback Function
+//     return Math.PI * radius * radius;
+// }
+
+// const circumference = function(radius) {
+//     return 2 * Math.PI * radius;
+// }
+
+// const diameter = function(radius) {
+//     return 2 * radius;
+// }
+
+// Array.prototype.calculate = function(logic) {}
+// const calculate = function(radius, logic) {  //Higher Order Function
+//     const output = [];
+//     for(let i = 0 ; i < radius.length ; i++){
+//         output.push(logic(radius[i]));
+//     }
+//     return output;
+// }
+
+// console.log(calculate(radius, area));
+// console.log(calculate(radius, circumference));
+// console.log(calculate(radius, diameter));
+
+//console.log(radius.map(area));
+//console.log(radius.calculate(area));
+
+//19
